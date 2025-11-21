53eac3cea078c11949797d89c3d02de3802b848e
Akumaa <valoumar0711@gmail.com>
2025-11-21 13:02:21 +0100

    [FIX] Fixed stlye.css implementation and added tailwind to the project

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/styles.css">
+    <link rel="stylesheet" href="./CSS/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Index</title>
 </head>
 <body>
-    
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
 </body>
 </html>
