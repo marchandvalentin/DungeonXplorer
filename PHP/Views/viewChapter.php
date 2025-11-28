a7d360503055650d5f8aa427020cba24503cecff
Akumaa <valoumar0711@gmail.com>
2025-11-28 13:54:16 +0100

    [FIX] Style inclusion

--- PHP/Views/viewChapter.php
+++ PHP/Views/viewChapter.php
@@ -6,13 +6,13 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="CSS/personaliseColors.css">
-
+    <script src="JS/script.js" defer></script>
     <title>DungeonXPlorer - Créez Votre Héros</title>
 </head>
 <body class="text-medieval-cream" style="background: linear-gradient(135deg, #0d0b0a 0%, #1a1614 50%, #0d0b0a 100%);">
     <?php include 'PHP/header.php'; ?>
     
-    <?php echo $hero; ?>
+    <?php echo $hero['name']; ?>
     
     <?php include 'PHP/footer.php'; ?>
 </body>
