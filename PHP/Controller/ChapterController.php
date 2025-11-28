88183d4997e4bc07ae57ed89e1c1c35b41ac7179
Akumaa <valoumar0711@gmail.com>
2025-11-28 14:09:05 +0100

    [FIX] The view should have the full hero now

--- PHP/Controller/ChapterController.php
+++ PHP/Controller/ChapterController.php
@@ -1,12 +1,16 @@
 <?php
+    require_once __DIR__ . '/../BDD/bdd_functions.php';
+
     class ChapterController{
-        function show($hero){
+        function show($hero_id){
             //if not logged in, redirect to login
             if (!isset($_SESSION['user_id'])) {
                 header('Location: /login');
                 exit;
             }
 
+            $hero = getHeroById($hero_id);
+            
             include __DIR__ . '/../Views/viewChapter.php';
         }
     }
